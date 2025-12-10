e94d02a39923ddbc7ab67493e5c2838994c47a42
Halimat Aminu <ha25adh@herts.ac.uk>
2025-12-10 11:00:44 +0100

    Updated Histogram: Added data, updated data subset

--- graph_scripts/Hist_new.R
+++ graph_scripts/Hist_new.R
@@ -1,10 +1,17 @@
+library(readr)
+Sleep_health_and_lifestyle_dataset <- read.csv("dataset/Sleep_health_and_lifestyle_dataset.csv")
+View(Sleep_health_and_lifestyle_dataset)
 cw2 <- Sleep_health_and_lifestyle_dataset
+colnames(cw2)[8] <- "stress_level"
 
-data_hist <- cw2[cw2$Occupation %in% c("Engineer", "Doctor"), ]
+data_hist <- subset(
+  cw2,
+  Occupation == "Doctor" | Occupation == "Engineer"
+)
 breaks_seq <- seq(1, 10, by = 1)
 
 hist(
-  data_hist$`Stress Level`,
+  data_hist$stress_level,
   breaks = breaks_seq,
   main = "Stress Level (Engineers and Doctors)",
   xlab = "Stress Level",
@@ -28,5 +35,5 @@ axis(
 
 table(cw2$`Stress Level`[cw2$Occupation == "Engineer"])
 table(cw2$`Stress Level`[cw2$Occupation == "Doctor"])
-
 sum(cw2$Occupation == "Doctor" & cw2$`Stress Level` == 3)
+
