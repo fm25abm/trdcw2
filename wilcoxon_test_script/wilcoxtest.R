55443615fa288638be7c466b4a18223f02958540
fm25abm <fm25abm@herts.ac.uk>
2025-12-10 16:35:22 +0000

    Added comments for better clarity

--- wilcoxon_test_script/wilcoxtest.R
+++ wilcoxon_test_script/wilcoxtest.R
@@ -1,7 +1,11 @@
+#Reference dataset as df
 df <- read.csv("dataset/Sleep_health_and_lifestyle_dataset.csv")
 
+#Creates a subset of rows with only doctor or engineer as occupation
 subset_df <- subset(df, Occupation == "Doctor" | Occupation == "Engineer")
 
+#Renaming the 8th column to Stress for better interpretation of the test
 colnames(subset_df)[8]<- "Stress"
 
+#Runs the Wilcoxon Rank-Sum Test
 wilcox.test(Stress ~ Occupation, data=subset_df)
