71398ee27dc5415c261d55cf6fea8e884127d6bc
Halimat Aminu <ha25adh@herts.ac.uk>
2025-12-11 01:33:45 +0100

    Addition of comments

--- graph_scripts/boxplot.R
+++ graph_scripts/boxplot.R
@@ -1,11 +1,14 @@
 library(readr)
 Sleep_health_and_lifestyle_dataset <- read.csv("dataset/Sleep_health_and_lifestyle_dataset.csv")
 View(Sleep_health_and_lifestyle_dataset)
+# Adding _ to column so I can call it without errors due to space
 colnames(Sleep_health_and_lifestyle_dataset)[8] <- "stress_level"
+# Creating a subset of rows with only doctor or engineer as occupation
 new_df <- subset(
   Sleep_health_and_lifestyle_dataset, 
   Occupation == "Doctor" | Occupation == "Engineer"
 )
+# Creates a Boxplot for stress levels comparison between doctors and engineers
 boxplot(
   stress_level~Occupation, 
   data = new_df, 
@@ -14,6 +17,7 @@ boxplot(
   ylab = "Stress Levels",
   col = c("lightblue", "lightgreen")
 )
+#Creates Legends for clearer interpretation of the graph
 legend("topright",
        legend = c("Doctors' Stress Levels (Age: 27-59)", "Engineers' Stress Levels (Age:27-59)"),
        fill = c("lightblue", "lightgreen"),
@@ -22,3 +26,4 @@ legend("topright",
        box.col = "black",
        cex = 0.8
 )
+
